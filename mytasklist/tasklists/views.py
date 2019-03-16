70f9889274479e8e9c5126480293e8435f64945e
Carlos Arruda <caugustogarruda@gmail.com>
2019-03-16 14:06:19 -0300

    Validar login, redirecionar e exibir erros

--- mytasklist/tasklists/views.py
+++ mytasklist/tasklists/views.py
@@ -2,6 +2,22 @@ from django.shortcuts import render
 
 from django.views import View
 
+from django.contrib.auth.models import User
+
 class Login(View):
+    """
+        autor: Carlos Arruda
+        criado em: 03/2019
+    """
     def get(self, request, *args, **kwargs):
         return render(request, 'login.html')
+
+    def post(self, request, *args, **kwargs):
+        email = request.POST['email']
+        validar = User.objects.filter(email=email)
+        if validar:
+            return render(request, 'dashboard.html')
+        else:
+            return render(request, 'login.html',{
+                "erros": "Você não possui uma conta cadastrada"
+            })
